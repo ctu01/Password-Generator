349a964ec11aeed340374c0012f6f83774060060
Colin <colintu01@gmail.com>
2019-06-13 00:16:51 +1000

    Commiting pass_gen

---
+++ pass_gen.py
@@ -0,0 +1,16 @@
+#importing random and string module
+import random
+import string
+
+s_char = '!@#$%^&*.-_'
+chars = (string.ascii_letters + string.digits + s_char)
+
+length = input("Insert password length: ")
+length = int(length)
+
+password = ''
+for c in range(length):
+    password += random.choice(chars)
+print("Your password is: {}. Write it down!".format(password))
+# Will replace {} with password
+
